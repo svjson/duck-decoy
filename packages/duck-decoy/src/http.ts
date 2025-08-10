4a0b3a236bff6ea198614604ea93b0c356ab4521
Sven Johansson <sven.johansson@iteam.se>
2025-08-10 23:52:46 +0200

    jsdoc for http module

--- packages/duck-decoy/src/http.ts
+++ packages/duck-decoy/src/http.ts
@@ -1,16 +1,10 @@
 import { DecoyServer } from './server'
 import { RouteDef } from './types'
 
-class NoImplementationError extends Error {
-  constructor(message: string) {
-    super(message)
-  }
-}
-
-export interface HttpServerStartOptions {
-  port: number
-}
-
+/**
+ * HttpTransport interface implemented by adapters for actual
+ * HTTP libraries, e.g, Fastify
+ */
 export interface DuckDecoyHttpTransport {
   name: string
   registerRoute<State extends Object>(
@@ -22,6 +16,13 @@ export interface DuckDecoyHttpTransport {
   port(): number
 }
 
+/**
+ * Abstract base class serving as the abstraction of an HttpRequest
+ * internally in DuckDecoy and as a translation layer against the request
+ * abstraction of an actual Http library.
+ *
+ * Each Http Transport Provider is required to implement this class.
+ */
 export abstract class DuckDecoyRequest {
   abstract get body(): any
   abstract get pathParameters(): Record<string, string>
@@ -29,6 +30,13 @@ export abstract class DuckDecoyRequest {
   abstract get url(): string
 }
 
+/**
+ * Abstract base class serving as the abstraction of an HttpResponse
+ * internally in DuckDecoy and as a translation layer against the
+ * response abstraction of an actual Http library.
+ *
+ * Each Http Transport Provider is required to implement his class.
+ */
 export abstract class DuckDecoyResponse {
   _code: number | undefined
   _body: any | undefined
@@ -46,12 +54,43 @@ export abstract class DuckDecoyResponse {
   abstract encode(): Promise<void>
 }
 
+/**
+ * Error thrown when initialization of a Http Transport is attempted
+ * but the implementation is not available.
+ *
+ * This can happen if the implementation is not installed or if the
+ * implementation is not a class or an instance of DuckDecoyHttpTransport.
+ */
+class NoImplementationError extends Error {
+  constructor(message: string) {
+    super(message)
+  }
+}
+
+export interface HttpServerStartOptions {
+  port: number
+}
+
 const isHttpTransportInstance = (candidate: any) => {
   if (typeof candidate === 'object') return true
 
   return false
 }
 
+/**
+ * Attempt to resolve the value provided for HttpTransport in a server
+ * configuration to an actual Http Transport implementation.
+ *
+ * This can be an instance of DuckDecoyHttpTransport or a class
+ * constructor function that can be instantiated to create an instance of
+ * DuckDecoyHttpTransport.
+ *
+ * @param configValue - The value provided for HttpTransport in the server configuration
+ *
+ * @return A Promise that resolves to an instance of DuckDecoyHttpTransport
+ *
+ * @throws NoImplementationError if the provided value is not an instance
+ */
 export const resolveHttpTransport = async (
   configValue: string | undefined | DuckDecoyHttpTransport | Function
 ): Promise<DuckDecoyHttpTransport> => {
